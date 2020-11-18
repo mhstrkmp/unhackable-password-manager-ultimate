7835d66124f77eb1320bfaf5a9552dbbdf3d1b49
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-11-18 17:31:46 +0100

    :heavy_plus_sign: Add react-hook-form

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 import { getPassword } from "./api/passwords";
+import useAsync from "./hooks/useAsync";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [password, setPassword] = useState(null);
+  const { data, loading, error, doFetch } = useAsync(() =>
+    getPassword("banking")
+  );
+
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => {
+    getPassword(data);
+    console.log(getPassword(data));
+  };
+
+  console.log(watch("fetchPassword")); // watch input value by passing the name of it
 
   useEffect(() => {
-    const doFetch = async () => {
-      const newPassword = await getPassword("bike");
-      setPassword(newPassword);
-    };
     doFetch();
   }, []);
 
@@ -18,7 +26,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {password}
+        {loading && <div>Loading...</div>}
+        {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* include validation with required or other standard HTML validation rules */}
+          <input name="fetchPassword" ref={register({ required: true })} />
+          {/* errors will return when field validation fails  */}
+          {errors.fetchPassword && <span>This field is required</span>}
+
+          <input type="submit" />
+        </form>
+        );
       </header>
     </div>
   );
