7893427285bc0a608ddc6a38c46e463a32f6f05b
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-11-19 21:51:20 +0100

    :recycle: Refactor server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+/* require("dotenv").config();
 
 const express = require("express");
 const { getPassword, setPassword } = require("./lib/passwords");
@@ -68,4 +68,106 @@ async function run() {
     console.log(`UHP API listening at http://localhost:${port}`);
   });
 }
+run();
+ */
+
+require("dotenv").config();
+
+const express = require("express");
+
+const path = require("path");
+
+const {
+  getPassword,
+  setPassword,
+  getPasswords,
+  deletePassword,
+} = require("./lib/passwords");
+const { connect } = require("./lib/database");
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3600;
+
+app.get("/api/passwords/:name", async (request, response) => {
+  const { name } = request.params;
+  try {
+    const passwordValue = await getPassword(name);
+    if (!passwordValue) {
+      response
+        .status(404)
+        .send("Could not find the password you have specified");
+      return;
+    }
+    response.send(passwordValue);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send("An internal server error occured");
+  }
+});
+
+app.delete("/api/passwords/:name", async (request, response) => {
+  const { name } = request.params;
+  try {
+    const deleted = await deletePassword(name);
+    if (deleted.deletedCount === 0) {
+      response
+        .status(404)
+        .send("Could not find the password you have specified");
+      return;
+    }
+    response.json("Password deleted");
+  } catch (error) {
+    console.error(error);
+    response.status(500).send("An internal server error occured");
+  }
+});
+
+app.get("/api/passwords", async (request, response) => {
+  try {
+    const passwords = await getPasswords();
+    response.json(passwords);
+  } catch (error) {
+    console.error(error);
+    response
+      .status(500)
+      .send("An unexpected error occured. Please try again later!");
+  }
+});
+
+app.post("/api/passwords", async (request, response) => {
+  const password = request.body;
+
+  try {
+    await setPassword(password.name, password.value);
+    response.send(`Successfully set ${password.name} `);
+  } catch (error) {
+    console.error(error);
+    response
+      .status(500)
+      .send("An unexpected error occured. Please try again later!");
+  }
+});
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+async function run() {
+  console.log("Connecting to database...");
+  await connect(process.env.MONGO_DB_URI, process.env.MONGO_DB_NAME);
+  console.log("Connected to database 🎉");
+
+  app.listen(port, () => {
+    console.log(`PW4U API listening at http://localhost:${port}`);
+  });
+}
+
 run();
